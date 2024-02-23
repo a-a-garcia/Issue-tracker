6de0c2f5876be3be671ec16f48438f7ee3a559c1
Anthony <angarce01@gmail.com>
2024-02-22 16:39:01 -0800

    add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import { Card, Heading, Text } from "@radix-ui/themes";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Flex } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
 
 // this is of type string because data coming from the URL is always a string, we must parse it into a number
 interface Props {
@@ -20,10 +24,14 @@ const IssueDetailPage = async ({ params }: Props) => {
 
     return (
       <div>
-        <p>{issue.title}</p>
-        <p>{issue.description}</p>
-        <p>{issue.status}</p>
-        <p>{issue.createdAt.toDateString()}</p>
+        <Heading>{issue.title}</Heading>
+        <Flex className="space-x-3" my="2">
+            <IssueStatusBadge status={issue.status}/>
+            <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose mt-4">
+            <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
       </div>
     );
 };

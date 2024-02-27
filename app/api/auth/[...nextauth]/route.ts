fca46d108338f17b63160dbe1904447090f865fc
Anthony <angarce01@gmail.com>
2024-02-26 18:22:05 -0800

    implement google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,14 @@
 import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
 
 const handler = NextAuth({
-  providers: []
+  providers: [
+    GoogleProvider({
+        // need `!` after each of these to tell next.js we ensured that these values exist
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
+  ]
 })
 
 export { handler as GET, handler as POST }

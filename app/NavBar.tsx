d57f50ba3d1e55fbd19f5cc55b868c4a58d9d63d
Anthony <angarce01@gmail.com>
2024-02-29 11:24:57 -0800

    change layout of navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,53 +1,66 @@
-'use client'
-import React from 'react'
-import Link from 'next/link'
-import { AiFillBug } from "react-icons/ai"
-import classnames from 'classnames'
-import { usePathname } from 'next/navigation'
-import { Box } from '@radix-ui/themes'
-import { useSession } from 'next-auth/react';
+"use client";
+import React from "react";
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+import { usePathname } from "next/navigation";
+import { Box, Container, Flex } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
 
 const NavBar = () => {
   const { status, data: session } = useSession();
-  const currentPath = usePathname()
+  const currentPath = usePathname();
 
   const navLinks = [
     {
       name: "Dashboard",
-      href: "/dashboard"
+      href: "/dashboard",
     },
-    
+
     {
       name: "Issues",
-      href: "/issues/list"
-    }
-  ]
+      href: "/issues/list",
+    },
+  ];
 
   return (
-    <nav className="flex space-x-6 h-16 border-b items-center px-5 mb-5">
-      <ul className="flex space-x-6">
-        <AiFillBug />
-        {navLinks.map((link) => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              className={classnames({
-                'text-zinc-900': link.href === currentPath,
-                'text-zinc-300': link.href !== currentPath,
-                'hover:text-zinc-800 transition-colors': true,
-              })}
-            >
-              {link.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && <Link href="/api/auth/signout">Log Out</Link>}
-        {status === "unauthenticated" && <Link href="/api/auth/signin">Log In</Link>}
-      </Box>
+    <nav className="py-3 border-b px-5 mb-5">
+      {/* wrapping everything in a Container here will align the content of our navbar with our page contents */}
+      <Container>
+        <Flex justify={"between"}>
+          <Flex align="center" gap="3">
+            <ul className="flex space-x-6">
+              <Link href="/">
+                <AiFillBug />
+              </Link>
+              {navLinks.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    className={classnames({
+                      "text-zinc-900": link.href === currentPath,
+                      "text-zinc-300": link.href !== currentPath,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Log Out</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Log In</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;

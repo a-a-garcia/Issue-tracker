a6026c1ba572f04b0cb2f2cb5e1af68ec784ce90
Anthony <angarce01@gmail.com>
2024-03-01 15:00:15 -0800

    enhance API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth"
@@ -6,26 +6,42 @@ import { getServerSession } from "next-auth"
 
 // takes two parameters: request of type NextRequest and an object { params } where params is an object with a property params containing an object with an id property.
 export async function PATCH(request: NextRequest, { params }: { params: {id: string }} ) {
-    const session = await getServerSession();
+    // you can comment out 10-16 to test patch with assigning
+    // const session = await getServerSession();
 
-    if (!session) {
-        // 401 means unauthorized
-        return NextResponse.json
-        ({}, { status: 401 })
-    }
+    // if (!session) {
+    //     // 401 means unauthorized
+    //     return NextResponse.json
+    //     ({}, { status: 401 })
+    // }
 
     //wait for a response from the server
     const response = await request.json()
     //validate
-    const validation = issueSchema.safeParse(response)
+    const validation = patchIssueSchema.safeParse(response)
     //if validation fails...
     if (!validation.success) {
         return NextResponse.json(validation.error.format(), { status: 400})
     }
-    //if validation succeeds, use prisma update to find the issue with the id and update it with the data from the response body
+
+    // if there is a userId in the body of the request, we want to make sure that user exists
+    // destructure these properties from the response body
+    const {assignedToUserId, title, description} = response;
+    if (assignedToUserId) {
+        const user = await prisma.user.findUnique({where: {id: assignedToUserId } })
+        if (!user) {
+            return NextResponse.json({error: 'Invalid User'}, {status: 400})
+        }
+    }
+
+    //if validation succeeds and a user is found, use prisma update to find the issue with the id and update it with the data from the response body
     const updatedIssue = await prisma.issue.update({
         where: { id: parseInt(params.id)},
-        data: { title: response.title, description: response.description}
+        data: { 
+            title: title, 
+            description: description,
+            assignedToUserId
+        }
     })
     // return the updated issue back to the client
     return NextResponse.json(updatedIssue, { status: 200 })
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -4,3 +4,22 @@ export const issueSchema = z.object({
     title: z.string().min(1, "Title must contain at least one character").max(255),
     description: z.string().min(1, "Description must contain as least one character.")
 });
+
+export const patchIssueSchema = z.object({
+    title: z
+    .string()
+    .min(1, "Title must contain at least one character")
+    .max(255)
+    .optional(),
+    description: z
+    .string()
+    .min(1, "Description must contain as least one character.")
+    .max(65535)
+    .optional(),
+    assignedToUserId: z
+    .string()
+    .min(1, "assignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable()
+})

bc2a7fad984dce38f872e53cdccfab983a3b4767
Anthony <angarce01@gmail.com>
2024-03-01 17:13:44 -0800

    refactor asignee select component

--- app/issues/[id]/AsigneeSelect.tsx
+++ app/issues/[id]/AsigneeSelect.tsx
@@ -1,31 +1,15 @@
 "use client";
-import React from "react";
-import { Select } from "@radix-ui/themes";
-import { Issue, User } from "@prisma/client";
-import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
 import Skeleton from "@/app/components/Skeleton";
+import { Issue, User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 
 const AsigneeSelect = ({ issue }: { issue: Issue }) => {
   //   destructure the result to grab 3 properties
   //   data - renamed as users, error & isLoading
-  const { data: users, error, isLoading } =
-    // call useQuery and give it an object with 2 properties (also type it as an array of Users)
-    useQuery<User[]>({
-      // used for uniquely identifying a piece of data in the cache - array of strings
-      queryKey: ["users"],
-
-      // a function for fetching data - can use any fetch function
-      // returns a promise, we want to return the data thats resolved from the promise (hence the .then)
-      queryFn: () => axios.get("/api/users").then((res) => res.data),
-
-      // defines how long to treat fetched data as fresh. Once stale, it will re-fetch. default is 0, which we don't want to take advantage of caching
-      staleTime: 60 * 1000,
-
-      // defines how many times to retry if the call to the backend fails. (it will try retry amount of times + 1 the inital call)
-      retry: 3,
-    });
+  const { data: users, error, isLoading } = useUsers();
 
   // return a skeleton while fetching users
   if (isLoading) return <Skeleton />;
@@ -47,20 +31,20 @@ const AsigneeSelect = ({ issue }: { issue: Issue }) => {
   //     fetchUsers();
   //   }, []);
 
+  const assignIssue = async (userId: string) => {
+    await axios
+      .patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId === "Unassigned" ? null : userId,
+      })
+      .catch(() => {
+        toast.error("Failed to assign issue.");
+      });
+  };
+
   return (
     <>
       <Toaster />
-      <Select.Root
-        onValueChange={async (userId) => {
-          await axios
-            .patch(`/xapi/issues/${issue.id}`, {
-              assignedToUserId: userId === "Unassigned" ? null : userId,
-            })
-            .catch(() => {
-              toast.error("Failed to assign issue.");
-            });
-        }}
-      >
+      <Select.Root onValueChange={assignIssue}>
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
           <Select.Group>
@@ -82,5 +66,22 @@ const AsigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () =>
+  // call useQuery and give it an object with 2 properties (also type it as an array of Users)
+  useQuery<User[]>({
+    // used for uniquely identifying a piece of data in the cache - array of strings
+    queryKey: ["users"],
+
+    // a function for fetching data - can use any fetch function
+    // returns a promise, we want to return the data thats resolved from the promise (hence the .then)
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+
+    // defines how long to treat fetched data as fresh. Once stale, it will re-fetch. default is 0, which we don't want to take advantage of caching
+    staleTime: 3600 * 1000,
+
+    // defines how many times to retry if the call to the backend fails. (it will try retry amount of times + 1 the inital call)
+    retry: 3,
+  });
 
 export default AsigneeSelect;
